dc356ac0174cc774c39961de336a36125a73d3b4
Erzaa <vanya123423@yandex.ru>
2026-05-18 23:36:45 +0700

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,9 +104,8 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
 }
 
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOWED_ORIGINS = [origin.strip() for origin in os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')]
+CORS_ALLOW_CREDENTIALS = True 
 
 import dj_database_url
 DATABASE_URL = os.getenv('DATABASE_URL')
